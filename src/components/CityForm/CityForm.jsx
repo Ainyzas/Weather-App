fc6ee23e88c5429126a284c3e3ba85e79d8a7b1d
ainas <puidokasainas@gmail.com>
2023-04-05 17:05:40 +0300

    eslint config comment

--- src/components/CityForm/CityForm.jsx
+++ src/components/CityForm/CityForm.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import React, { useState, useEffect } from 'react';
 import { getWeatherByCity, getWeatherByCoordinates } from '../../api-calls/weather';
 import { getForecastByCity, getForecastByCoordinates } from '../../api-calls/forecasts';
